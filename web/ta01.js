67a1b8f57642a445fdbd78d0bb7bbdc208ee80bd
ploien <oie17001@byui.edu>
2019-04-30 12:43:35 -0600

    added color change button and corresponding js function

--- web/ta01.js
+++ web/ta01.js
@@ -1,3 +1,9 @@
 function clickMe() {
    alert("Clicked!");
 }
+
+function changeColor {
+	
+	var color = document,getElementById("colorInput").value;
+	document.getElementById("div1").style.backgroundColor = color;
+}
