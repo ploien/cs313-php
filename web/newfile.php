4bd82749928cf31ee4ce9fb17ea988dfda9d6db8
ploien <oie17001@byui.edu>
2019-05-04 16:37:30 -0600

    Added flex column around three main sections on newpage.php

--- web/newfile.php
+++ web/newfile.php
@@ -7,65 +7,69 @@
   <body>
     <?php include 'header.php';?>
     
-    <h2 id="aboutMeHeader">About Me</h2>
-    <p class="aboutMe">I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering/</p>
-    <p class="aboutMe">My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home.</p>
-    
-    <h2 id="interestHeader">Interests</h2>
-    <p>One of my favorite passtimes has always been reading. I particularly enjoy reading Fantasy books. Below I've listed a couple of the laatest series that I've read, as well as one of my all time favorites</p>
-    
-    <div class="flex-row">
-    
+    <div class="flex-column">
         <div>
-          <h3><a href="https://brandonsanderson.com/books/the-stormlight-archive/">The Stormlight Archive</a></h3>
-          <h4>The Way of Kings</h4>
-          <img src="the-way-of-kings.jpg">
-          <h4>Words of Radiance</h4>
-          <img src="words-of-radiance.jpg">
-          <h4>Oathbringer</h4>
-          <img src="Oathbringer-628rev-1-e1505943721525.jpg">
+            <h2 id="aboutMeHeader">About Me</h2>
+            <p class="aboutMe">I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering/</p>
+            <p class="aboutMe">My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home.</p>
         </div>
         
         <div>
-          <h3><a href="http://www.brentweeks.com/series/the-lightbringer-series/">Lightbringer Series</a></h3>
-          <h4>The Black Prism</h4>
-          <img src="Weeks_Black-Prism-TP.jpg">
-          <h4>The Blinding Knife</h4>
-          <img src="Weeks_BlindingKnife-TP.jpg">
-          <h4>The Broken Eye</h4>
-          <img src="Weeks-BrokenEye-Animated_08-26-14.gif">
-          <h4>The Blood Mirror</h4>
-          <img src="Weeks_BloodMirror-TP.jpg">
+            <h2 id="interestHeader">Interests</h2>
+            <p>One of my favorite passtimes has always been reading. I particularly enjoy reading Fantasy books. Below I've listed a couple of the laatest series that I've read, as well as one of my all time favorites</p>
         </div>
         
-        <div>
-          <h3><a href="http://www.redwall.org/books.html">The Redwall Adventures</a></h3>
-          <ul>
-           <li>Redwall (1986)</li>
-           <li>Mossflower (1988)</li>
-           <li>Mattimeo (1989)</li>
-           <li>Mariel of Redwall (1991)</li>
-           <li>Salamandastron (1992)</li>
-           <li>Martin the Warrior (1993)</li>
-           <li>The Bellmaker (1994)</li>
-           <li>Outcast of Redwall (1995)</li>
-           <li>The Pearls of Lutra (1996)</li>
-           <li>The Long Patrol (1997)</li>
-           <li>Marlfox (1998)</li>
-           <li>The Legend of Luke (1999)</li>
-           <li>Lord Brocktree (2000)</li>
-           <li>The Taggerung (2001)</li>
-           <li>Triss (2002)</li>
-           <li>Loamhedge (2003)</li>
-           <li>Rakkety Tam (2004)</li>
-           <li>High Rhulain (2005)</li>
-           <li>Eulalia! (2007)</li>
-           <li>Doomwyte (2008)</li>
-           <li>The Sable Quean (2010)</li>
-          </ul>
-        </div>
-        
-	</div>
+        <div class="flex-row">
+            <div>
+              <h3><a href="https://brandonsanderson.com/books/the-stormlight-archive/">The Stormlight Archive</a></h3>
+              <h4>The Way of Kings</h4>
+              <img src="the-way-of-kings.jpg">
+              <h4>Words of Radiance</h4>
+              <img src="words-of-radiance.jpg">
+              <h4>Oathbringer</h4>
+              <img src="Oathbringer-628rev-1-e1505943721525.jpg">
+            </div>
+            
+            <div>
+              <h3><a href="http://www.brentweeks.com/series/the-lightbringer-series/">Lightbringer Series</a></h3>
+              <h4>The Black Prism</h4>
+              <img src="Weeks_Black-Prism-TP.jpg">
+              <h4>The Blinding Knife</h4>
+              <img src="Weeks_BlindingKnife-TP.jpg">
+              <h4>The Broken Eye</h4>
+              <img src="Weeks-BrokenEye-Animated_08-26-14.gif">
+              <h4>The Blood Mirror</h4>
+              <img src="Weeks_BloodMirror-TP.jpg">
+            </div>
+            
+            <div>
+              <h3><a href="http://www.redwall.org/books.html">The Redwall Adventures</a></h3>
+              <ul>
+               <li>Redwall (1986)</li>
+               <li>Mossflower (1988)</li>
+               <li>Mattimeo (1989)</li>
+               <li>Mariel of Redwall (1991)</li>
+               <li>Salamandastron (1992)</li>
+               <li>Martin the Warrior (1993)</li>
+               <li>The Bellmaker (1994)</li>
+               <li>Outcast of Redwall (1995)</li>
+               <li>The Pearls of Lutra (1996)</li>
+               <li>The Long Patrol (1997)</li>
+               <li>Marlfox (1998)</li>
+               <li>The Legend of Luke (1999)</li>
+               <li>Lord Brocktree (2000)</li>
+               <li>The Taggerung (2001)</li>
+               <li>Triss (2002)</li>
+               <li>Loamhedge (2003)</li>
+               <li>Rakkety Tam (2004)</li>
+               <li>High Rhulain (2005)</li>
+               <li>Eulalia! (2007)</li>
+               <li>Doomwyte (2008)</li>
+               <li>The Sable Quean (2010)</li>
+              </ul>
+            </div> 
+    	</div>
+    </div>
 	
   </body>
 </html>
