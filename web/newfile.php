11221debe4d61f8816dbd2c943bc0da87ebfef3e
ploien <oie17001@byui.edu>
2019-05-04 00:39:15 -0600

    Set booklists in flex container

--- web/newfile.php
+++ web/newfile.php
@@ -14,30 +14,32 @@
     <h2 id="interestHeader">Interests</h2>
     <p>One of my favorite passtimes has always been reading. I particularly enjoy reading Fantasy books. Below I've listed a couple of the laatest series that I've read, as well as one of my all time favorites</p>
     
-    <div>
-      <h3><a href="https://brandonsanderson.com/books/the-stormlight-archive/">The Stormlight Archive</a></h3>
-      <h4>The Way of Kings</h4>
-      <img src="the-way-of-kings.jpg">
-      <h4>Words of Radiance</h4>
-      <img src="words-of-radiance.jpg">
-      <h4>Oathbringer</h4>
-      <img src="Oathbringer-628rev-1-e1505943721525.jpg">
-    </div>
-    
-    <div>
-      <h3><a href="http://www.brentweeks.com/series/the-lightbringer-series/">Lightbringer Series</a></h3>
-      <h4>The Black Prism</h4>
-      <img src="Weeks_Black-Prism-TP.jpg">
-      <h4>The Blinding Knife</h4>
-      <img src="Weeks_BlindingKnife-TP.jpg">
-      <h4>The Broken Eye</h4>
-      <img src="Weeks-BrokenEye-Animated_08-26-14.gif">
-      <h4>The Blood Mirror</h4>
-      <img src="Weeks_BloodMirror-TP.jpg">
-    </div>
-    
-    <div>
-      <h3><a href="http://www.redwall.org/books.html">The Redwall Adventures</a></h3>
-    </div>
+    <div class="flex-row">
+        <div>
+          <h3><a href="https://brandonsanderson.com/books/the-stormlight-archive/">The Stormlight Archive</a></h3>
+          <h4>The Way of Kings</h4>
+          <img src="the-way-of-kings.jpg">
+          <h4>Words of Radiance</h4>
+          <img src="words-of-radiance.jpg">
+          <h4>Oathbringer</h4>
+          <img src="Oathbringer-628rev-1-e1505943721525.jpg">
+        </div>
+        
+        <div>
+          <h3><a href="http://www.brentweeks.com/series/the-lightbringer-series/">Lightbringer Series</a></h3>
+          <h4>The Black Prism</h4>
+          <img src="Weeks_Black-Prism-TP.jpg">
+          <h4>The Blinding Knife</h4>
+          <img src="Weeks_BlindingKnife-TP.jpg">
+          <h4>The Broken Eye</h4>
+          <img src="Weeks-BrokenEye-Animated_08-26-14.gif">
+          <h4>The Blood Mirror</h4>
+          <img src="Weeks_BloodMirror-TP.jpg">
+        </div>
+        
+        <div>
+          <h3><a href="http://www.redwall.org/books.html">The Redwall Adventures</a></h3>
+        </div>
+	</div>
   </body>
 </html>
