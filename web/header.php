59099ba27d1ba4ca548be215b9654de0c72931e2
ploien <oie17001@byui.edu>
2019-05-03 23:15:23 -0600

    Intital pages for Week 2 Ponder

--- web/header.php
+++ web/header.php
@@ -1,3 +1,3 @@
 <?php
-  echo "<header id=header><h1>Peter's Home Page</h1></header>";
+  echo "<header id=header><h1>Peter's Home Page</h1><a href=newfile.php>Home Page</a><a href=assignment.php>Assignments Page</a></header>";
 ?>
