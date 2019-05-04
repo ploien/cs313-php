b85f5bcbfd515d812cb36281f3ef9e095077af88
ploien <oie17001@byui.edu>
2019-05-04 00:10:24 -0600

    Intital pages for Week 2 Ponder

--- web/newfile.php
+++ web/newfile.php
@@ -7,17 +7,29 @@
     <?php include 'header.php';?>
     
     <h2 id="aboutMeHeader">About Me</h2>
-    <p class="aboutMe">I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering</p>
-    <p class="aboutMe">My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home</p>
+    <p class="aboutMe">I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering/</p>
+    <p class="aboutMe">My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home.</p>
     
     <h2 id="interestHeader">Interests</h2>
-    <p>One of my favorite passtimes has always been reading. I particularly enjoye reading Fantasy books. Below I've listed some of my favorite Fantasy series.</p>
+    <p>One of my favorite passtimes has always been reading. I particularly enjoy reading Fantasy books. Below I've listed a couple of the laatest series that I've read, as well as one of my favorites</p>
     
     <div>
       <h3><a href="https://brandonsanderson.com/books/the-stormlight-archive/">The Stormlight Archive</a></h3>
       <h4>The Way of Kings</h4>
       <h4>Words of Radiance</h4>
       <h4>Oathbringer</h4>
+    </div>
+    
+    <div>
+      <h3><a href="http://www.brentweeks.com/series/the-lightbringer-series/">Lightbringer Series</a></h3>
+      <h4>The Black Prism</h4>
+      <h4>The Blinding Knife</h4>
+      <h4>The Broken Eye</h4>
+      <h4>The Blood Mirror</h4>
+    </div>
+    
+    <div>
+      <h3><a href="http://www.redwall.org/books.html">The Redwall Adventures</a></h3>
     </div>
   </body>
 </html>
