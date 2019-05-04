9e3fb7085e5cf8f0d0818b4a5ab211bd9fa21a59
ploien <oie17001@byui.edu>
2019-05-03 23:58:41 -0600

    Intital pages for Week 2 Ponder

--- web/newfile.php
+++ web/newfile.php
@@ -5,5 +5,19 @@
   </head>
   <body>
     <?php include 'header.php';?>
+    
+    <h2 id="aboutMeHeader">About Me</h2>
+    <p class="aboutMe">I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering</p>
+    <p class="aboutMe">My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home</p>
+    
+    <h2 id="interestHeader">Interests</h2>
+    <p>One of my favorite passtimes has always been reading. I particularly enjoye reading Fantasy books. Below I've listed some of my favorite Fantasy series.</p>
+    
+    <div>
+      <h3><a href="https://brandonsanderson.com/books/the-stormlight-archive/">The Stormlight Archive</a></h3>
+      <h4>The Way of Kings</h4>
+      <h4>Words of Radiance</h4>
+      <h4>Oathbringer</h4>
+    </div>
   </body>
 </html>
