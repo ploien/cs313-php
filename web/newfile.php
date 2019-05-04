4c137dedc60f6e70cf932a3698496920b1402725
ploien <oie17001@byui.edu>
2019-05-04 00:47:36 -0600

    Added Booknames to redwall list

--- web/newfile.php
+++ web/newfile.php
@@ -15,6 +15,7 @@
     <p>One of my favorite passtimes has always been reading. I particularly enjoy reading Fantasy books. Below I've listed a couple of the laatest series that I've read, as well as one of my all time favorites</p>
     
     <div class="flex-row">
+    
         <div>
           <h3><a href="https://brandonsanderson.com/books/the-stormlight-archive/">The Stormlight Archive</a></h3>
           <h4>The Way of Kings</h4>
@@ -39,7 +40,32 @@
         
         <div>
           <h3><a href="http://www.redwall.org/books.html">The Redwall Adventures</a></h3>
+          <ul>
+           <li>Redwall (1986)</li>
+           <li>Mossflower (1988)</li>
+           <li>Mattimeo (1989)</li>
+           <li>Mariel of Redwall (1991)</li>
+           <li>Salamandastron (1992)</li>
+           <li>Martin the Warrior (1993)</li>
+           <li>The Bellmaker (1994)</li>
+           <li>Outcast of Redwall (1995)</li>
+           <li>The Pearls of Lutra (1996)</li>
+           <li>The Long Patrol (1997)</li>
+           <li>Marlfox (1998)</li>
+           <li>The Legend of Luke (1999)</li>
+           <li>Lord Brocktree (2000)</li>
+           <li>The Taggerung (2001)</li>
+           <li>Triss (2002)</li>
+           <li>Loamhedge (2003)</li>
+           <li>Rakkety Tam (2004)</li>
+           <li>High Rhulain (2005)</li>
+           <li>Eulalia! (2007)</li>
+           <li>Doomwyte (2008)</li>
+           <li>The Sable Quean (2010)</li>
+          </ul>
         </div>
+        
 	</div>
+	
   </body>
 </html>
