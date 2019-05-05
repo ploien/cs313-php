1fd7051c8a549b6832be5a7f2e7162a8c77f8fff
ploien <oie17001@byui.edu>
2019-05-04 21:29:45 -0600

    Added colors to main page

--- web/newfile.php
+++ web/newfile.php
@@ -10,13 +10,17 @@
     <div class="flex-column">
         <div>
             <h2 id="aboutMeHeader">About Me</h2>
-            <p class="aboutMe">I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering/</p>
-            <p class="aboutMe">My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home.</p>
+            <div class="text">
+            	<p>I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering/</p>
+            	<p>My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home.</p>
+            </div>
         </div>
         
         <div>
             <h2 id="interestHeader">Interests</h2>
-            <p>One of my favorite passtimes has always been reading. I particularly enjoy reading Fantasy books. Below I've listed a couple of the laatest series that I've read, as well as one of my all time favorites</p>
+            <div class="text">
+            	<p class="about">One of my favorite passtimes has always been reading. I particularly enjoy reading Fantasy books. Below I've listed a couple of the laatest series that I've read, as well as one of my all time favorites</p>
+            </div>
         </div>
         
         <div class="flex-row">
