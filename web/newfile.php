399759c13f88b6fff96efe4e30f2c69ec26913dd
ploien <oie17001@byui.edu>
2019-05-04 21:32:23 -0600

    Added colors to main page

--- web/newfile.php
+++ web/newfile.php
@@ -11,8 +11,8 @@
         <div>
             <h2 id="aboutMeHeader">About Me</h2>
             <div class="text">
-            	<p>I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering/</p>
-            	<p>My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home.</p>
+            	<p>I'm originally from Seattle, WA but currently live in Rexburg, ID in order to attend school and major in Software Engineering.
+            	My wife is from Mexico City and I served  a Mission in Oaxaca, Mexico, so we enjoy speaking Spanish with each other at home.</p>
             </div>
         </div>
         
