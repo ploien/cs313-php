d0937ed99e8839a307a32c36a5b7c0352500029e
ploien <oie17001@byui.edu>
2019-05-21 21:55:13 -0600

    ta05

--- web/ta05/scriptureDetails.php
+++ web/ta05/scriptureDetails.php
@@ -14,10 +14,11 @@
 	echo "<h1>Scripture Resources</h1>";
      
 	
-	$db->query('SELECT content FROM scriptures WHERE id=' . $_GET["id"]) as $row)
-	{
-		//echo '<b>' . $row2['book'] . ' </b>' . $row2['chapter'] . ':' . $row2['verse'] . ' - "' . $row2['content'] . '"<br><br>';
-	}
+	$db->query('SELECT content FROM scriptures WHERE id=' . $_GET["id"] . ';') as $row;
+
+	
+	//echo '<b>' . $row['book'] . ' </b>' . $row['chapter'] . ':' . $row['verse'] . ' - "' . $row['content'] . '"<br><br>';
+
 	
 
 ?> 
