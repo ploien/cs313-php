5d3e773ddd6d52b5aeac5471b1c138084c0e7b25
ploien <oie17001@byui.edu>
2019-05-29 21:09:23 -0600

    Initial pages for Project 1

--- db/myDb.sql
+++ db/myDb.sql
@@ -40,11 +40,6 @@ CREATE TABLE vehicle (
 	vehicle_year integer NOT NULL
 );
 
-ALTER TABLE vehicle 
-ADD COLUMN work_order_id integer REFERENCES work_order,
-ADD COLUMN owner_id integer REFERENCES owner;
-
-
 CREATE TABLE work_order (
 	work_order_id SERIAL UNIQUE PRIMARY KEY,
 	labor_hours float(2),
@@ -52,11 +47,6 @@ CREATE TABLE work_order (
 	start_date date NOT NULL,
 	end_date date
 );
-
-
-ALTER TABLE work_order 
-ADD COLUMN vin integer REFERENCES vehicle;
-
 
 CREATE TABLE part (
 	part_number SERIAL UNIQUE PRIMARY KEY,
@@ -68,10 +58,6 @@ CREATE TABLE part (
 	receipt_number text NOT NULL
 );
 
-ALTER TABLE part
-ADD COLUMN work_order_id integer REFERENCES work_order;
-
-
 CREATE TABLE owner (
 	owner_id SERIAL UNIQUE PRIMARY KEY,
 	name_last text NOT NULL,
@@ -80,7 +66,43 @@ CREATE TABLE owner (
 	email text,
 	address text
 );
+
+ALTER TABLE vehicle 
+ADD COLUMN work_order_id integer REFERENCES work_order,
+ADD COLUMN owner_id integer REFERENCES owner;
+
+ALTER TABLE work_order 
+ADD COLUMN vin integer REFERENCES vehicle;
+
+ALTER TABLE part
+ADD COLUMN work_order_id integer REFERENCES work_order;
+
+
+INSERT INTO owner (name_last, name_first, phone_number, email, address) VALUES 
+	('Wazowski', 'Mike', '234-412-1234', 'mike@hotmail.com', '123 Monster ln. Topeka, KS 81231'),
+	('Smith', 'Robert', '251-651-6122', 'robsmith@gmail.com', '651 Alpine St. Apt. 123 Tacoma, WA'),
+	('McDonald', 'Ronald', '531-231-5123', 'ronmcdon@mcdonalds.com', '451 Big Mac Ave. New York');
 	
+INSERT INTO vehicle (make, model, vehicle_year) VALUES
+	('Ford', 'Ranger', 1998),
+	('Mazda', 'Protege', 2002),
+	('Ferrari', '488 Spider', 2018),
+	('Land Rover', 'Range Rover', 2008);
+	
+UPDATE vehicle SET owner_id = (SELECT owner_id FROM owner WHERE (name_last = 'Smith' AND name_first = 'Robert'))
+WHERE (make = 'Ford' OR make = 'Mazda');
+
+UPDATE vehicle SET owner_id = (SELECT owner_id FROM owner WHERE (name_last = 'Wazowski' AND name_first = 'Mike'))
+WHERE (make = 'Land Rover');
+
+UPDATE vehicle SET owner_id = (SELECT owner_id FROM owner WHERE (name_last = 'McDonald' AND name_first = 'Ronald'))
+WHERE (make = 'Ferrari');
+
+INSERT INTO work_order (labor_hours, cost, start_date, vin) VALUES
+	(2, 240, current_date, (SELECT vin FROM vehicle WHERE model = 'Ranger')),
+	(3, 160, current_date, (SELECT vin FROM vehicle WHERE model = '488 Spider'));
+	
+DELETE FROM work_order WHERE vin IS NULL;
 /*
 Team Activity 05 queries
 */
@@ -104,19 +126,6 @@ INSERT INTO scriptures (book, chapter, verse, content)
 	
 INSERT INTO scriptures (book, chapter, verse, content)
 	VALUES ('Mosiah', 16, 9, 'He is the alight and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
-/*
-John 1:5
-And the light shineth in darkness; and the darkness comprehended it not.
-
-Doctrine and Covenants 88:49
-The alight shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall bcomprehend even God, being quickened in him and by him.
-
-Doctrine and Covenants 93:28
-He that akeepeth his commandments receiveth btruth and clight, until he is glorified in truth and dknoweth all things.
-
-Mosiah 16:9
-He is the alight and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.
-/*
 
 
 	
---
+++ web/Mechanic Project/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+   </head>
+   
+  <body>
+     <header><h1>Work Tracker</h1></header>
+     
+     <h2>Customer Directory</h2>
+     <?php
+        require_once 'connections.php';
+        
+        $statement = $db->query('SELECT name_last, name_first, address, phone FROM owner');
+        
+        $list = "<ul id=customerList>";
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+            $list .= '<li>' . $row['name_last'] . ',' . $row['name_first'] . ',' . $row['phone_number'] . '</li>';
+        }
+        
+        $list .= '</ul>';
+        
+        echo $list;
+     ?>
+     
+  </body>
+
+</html>
